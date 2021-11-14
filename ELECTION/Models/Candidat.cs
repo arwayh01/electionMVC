c59b57e41db3618be3f0825dfa0eb14c3de6bd2e
marwa-baklouti <marwa.baklouti66@gmail.com>
2021-11-14 20:35:42 +0200

    Scaffolding

---
+++ ELECTION/Models/Candidat.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ELECTION.Models
+{
+    public partial class Candidat
+    {
+        public Candidat()
+        {
+            Electeurs = new HashSet<Electeur>();
+        }
+
+        public int CandidatId { get; set; }
+        public string NomCandidat { get; set; }
+        public string PrenomCandidat { get; set; }
+        public string CinCandidat { get; set; }
+        public string ImageCandidat { get; set; }
+        public int? AdministrateurId { get; set; }
+
+        public virtual Administrateur Administrateur { get; set; }
+        public virtual ICollection<Electeur> Electeurs { get; set; }
+    }
+}
---
+++ ELECTION/Models/Electeur.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ELECTION.Models
+{
+    public partial class Electeur
+    {
+        public int ElecteurId { get; set; }
+        public string NomElecteur { get; set; }
+        public string PrenomElecteur { get; set; }
+        public string CinElecteur { get; set; }
+        public string GenreElecteur { get; set; }
+        public int? CentreElectionId { get; set; }
+        public int? CondidatcandidatId { get; set; }
+
+        public virtual CentreElection CentreElection { get; set; }
+        public virtual Candidat Condidatcandidat { get; set; }
+    }
+}
